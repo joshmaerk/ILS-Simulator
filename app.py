bc9112124a0dd21f687dbd65089cacd30905caaa
joshmaerk <49692374+joshmaerk@users.noreply.github.com>
2019-12-31 13:01:04 +0100

    Delete Header from template

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
     return render_template("index.html", data=load_data())
+
 
 def load_data():
     with open("data.json") as file:
         f = file.read()
     return json.loads(f)
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8000)
